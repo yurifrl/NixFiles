1fe0faf70033f416988154c85d7bf94cb23372c1
yuri <yurifl03@gmail.com>
2019-07-14 00:02:12 -0300

    :shipit:

---
+++ configuration.nix
@@ -0,0 +1,4 @@
+# This is the configuration.nix that you should put in /etc/nixos.
+# Replace device-name by the name of your device and /path/to/yarn with
+# the path to the yarn repository folder
+(import /home/yuri/yarn-nix/).device-name
---
+++ default.nix
@@ -0,0 +1,23 @@
+# # !!! This file belongs to the yarn framework. !!!
+# # !!! Do not change unless you know what you're doing. !!!
+
+let
+  # This function takes the path of a device module as an argument
+  # and returns a complete module to be imported in configuration.nix
+  makeDevice = devicePath: { config, pkgs, lib, ... }:
+
+  {
+    imports = [
+      devicePath
+    ];
+
+
+    system.stateVersion = "19.03";
+
+  };
+  deviceModules =
+    builtins.listToAttrs (map (deviceName: {
+      name = deviceName;
+      value = makeDevice (./devices + "/${deviceName}");
+    }) (import ./devices/all-devices.nix));
+in deviceModules
