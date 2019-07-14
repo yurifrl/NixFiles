db8277a6336c9b686426889d61765fb53e05dd55
yuri <yurifl03@gmail.com>
2019-07-14 19:03:14 -0300

    :sparkles:

--- core/default.nix
+++ core/default.nix
@@ -28,6 +28,7 @@ in {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    cabal-install
     gitAndTools.diff-so-fancy
     unstable.tmux
     kconf
